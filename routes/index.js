0abda41e36af386b4cbc4729b0b5bebafa21afb1
Jesse Furukawa <tuc64377@temple.edu>
2017-05-24 14:03:57 -0700

    add test express route and jade template

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: "G'day, World!" });
+});
+
 module.exports = router;
